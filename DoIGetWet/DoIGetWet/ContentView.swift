c8250921ac5efc38c3efb28a88c0ef9d3d330134
mSternEU <michael.sternicki@gmail.com>
2025-10-09 14:13:33 +0200

    adding Stacks and singular Content

--- DoIGetWet/DoIGetWet/ContentView.swift
+++ DoIGetWet/DoIGetWet/ContentView.swift
@@ -15,13 +15,25 @@ struct ContentView: View {
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
             
-            VStack{
-                Text("Lübeck")
+            VStack(spacing: 8){
+                Text("Lübeck, SH")
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundStyle(Color(.white))
 
-                    .frame(width: 200, height:200)
-                    .background(Color(.red))    //Modifiers wrap previous Content into own Views
+                    //.frame(width: 200, height:200)
+                    //.background(Color(.red))    // Modifiers wrap previous Content into own Views
+                    .padding()
+                VStack{
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180, alignment: .center)
+                    Text("11°C")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundStyle(Color.white)
+                }
+                Spacer()
             }
         }
     }
